1171318d245cc5bfdaee431f3edf2db762e26193
Titouan2b <titouan2bsur@gmail.com>
2024-02-06 10:58:13 +0100

    update mentorat

--- Front/src/components/Competence/Competence.jsx
+++ Front/src/components/Competence/Competence.jsx
@@ -5,7 +5,7 @@ import Slider from '../Slider/Slider.jsx'
 import { Animator, Fade, MoveIn, batch } from 'react-scroll-motion'
 
 export default function Competence() {
-  const [currentImageIndex, setCurrentImageIndex] = useState()
+  
 
   const slidesContent = [
     {
@@ -27,39 +27,14 @@ export default function Competence() {
   ]
 
   //changement d'image avec la progress bar
-  const [progress, setProgress] = useState()
-
-  useEffect(() => {
-    const animationDuration = 5
   
-
-  const changeImage = () => {
-    setCurrentImageIndex((prevIndex) => (prevIndex + 1) % slidesContent.length)
-    setProgress(0)
-  }
-
-  const startImageChangeTimeout = () =>{
-    setTimeout(changeImage, animationDuration * 1000)
-  }
-
-  setProgress(100)
-
-  const progressTimeout = setTimeout(startImageChangeTimeout, 0)
-
-  return() => {
-    clearTimeout(progressTimeout)
-  }
-  }, [currentImageIndex, slidesContent])
-
-  console.log(progress)
 
 
   return (
     <Animator animation={batch(Fade(0, 1), MoveIn(0, 50))}>
       <div className='competence'>
         <h2>Mes compétences</h2>
-        <div className='load-competence' style={{width: `${progress}$`}}>      
-        </div>
+        
         <Slider slides={slidesContent}/>
       </div>
     </Animator>
--- Front/src/components/Slider/Slider.jsx
+++ Front/src/components/Slider/Slider.jsx
@@ -5,8 +5,42 @@ import TypeIt from 'typeit-react'
 export default function Slider({slides}) {
 
 
+
     //changement d'image avec les flèches
     const [currentSlide, setCurrentSlide] = useState(0) 
+    const [progress, setProgress] = useState(0)
+    const [isPaused, setIsPaused] = useState(false)
+    // const [currentImageIndex, setCurrentImageIndex] = useState(0)
+
+    useEffect(() => {
+      const animationDuration = 5
+    
+
+      const changeImage = () => {
+        if(!isPaused){
+          setCurrentSlide((prevIndex) => (prevIndex + 1) % slides.length - 1)
+          // setProgress(0)
+          nextSlide()
+        }
+        
+        // console.log(currentImageIndex)
+      }
+
+      // const startImageChangeTimeout = () =>{
+      //   setInterval(changeImage, animationDuration * 1000)
+      // }
+
+      // setProgress(100)
+
+      // const progressTimeout = setInterval(startImageChangeTimeout, 0)
+      const interval = setInterval(changeImage, animationDuration * 1000)
+      return() => {
+       
+        clearInterval(interval)
+      }
+    }, [currentSlide])
+
+    // console.log(progress)
 
     const nextSlide = () => {
       setCurrentSlide((newSlide) => newSlide === slides.length -1 ? 0 : newSlide + 1)
@@ -17,36 +51,42 @@ export default function Slider({slides}) {
       setCurrentSlide((lastSlide) => lastSlide === 0 ? slides.length -1 : lastSlide - 1)
       console.log(currentSlide)
     }
+    
+
 
 
   return (
-    <div className="slider">
-        <button className='button-left' onClick={previousSlide}><i className="fa-solid fa-angle-left"></i></button>
+    <div>
+      <div className='load-competence' style={{width: `${progress}$`}}></div>
+      <div className="slider" onMouseEnter={() => setIsPaused(true)} onMouseLeave={() => setIsPaused(false)}>
+          <button className='button-left' onClick={previousSlide}><i className="fa-solid fa-angle-left"></i></button>
 
-        <div className='slider-content'>
-            <i className={slides[currentSlide].icone}></i>
-            {/* <TypeIt
-            className="Comp" 
-            as={"p"}
-            // getBeforeInit={(Comp) => {
+          <div className='slider-content'>
+              <i className={slides[currentSlide].icone}></i>
+              {/* <TypeIt
+              className="Comp" 
+              as={"p"}
+              // getBeforeInit={(Comp) => {
 
-            //   Comp.type(slides[currentSlide].paragraphe)
+              //   Comp.type(slides[currentSlide].paragraphe)
 
-            //   return Comp
-            // }}
-            
-            options={{
-              strings: [slides[currentSlide].paragraphe], 
-              speed: 100,
-              waitUnilVisible: true,
+              //   return Comp
+              // }}
               
-            }}
-            
-            /> */}
-            <p>{slides[currentSlide].paragraphe}</p>
-        </div>
-        <button className='button-right' onClick={nextSlide}><i className="fa-solid fa-angle-right"></i></button>
+              options={{
+                strings: [slides[currentSlide].paragraphe], 
+                speed: 100,
+                waitUnilVisible: true,
+                
+              }}
+              
+              /> */}
+              <p>{slides[currentSlide].paragraphe}</p>
+          </div>
+          <button className='button-right' onClick={nextSlide}><i className="fa-solid fa-angle-right"></i></button>
 
+      </div>
     </div>
+
   )
 }
